c4335f22e79be0e5e4b9bdbc80ed555a15904d6b
p0ttat0 <ericlin.zip@gmail.com>
2025-05-01 17:34:55 -0400

    vision initial commit

---
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class VisionSubsystem extends SubsystemBase {
+    public static NetworkTable limelight; 
+    
+    public void LimelightSystem() {
+        limelight = NetworkTableInstance.getDefault().getTable("limelight");
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(1);
+    }
+}
