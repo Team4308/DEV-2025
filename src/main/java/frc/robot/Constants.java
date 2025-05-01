edc8a33df42e72f98b3f1726aca4745e012020a0
Stephen <866628@pdsb.net>
2025-05-01 15:46:39 -0400

    Humble beggings / Start up

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,5 +4,18 @@ public final class Constants {
     public static final class LoggedDashboard {
         public static final boolean TUNING_MODE = false;
     }
-
+    
+    public static class Mapping {
+        public static class Drive {
+            public static int frontLeft = 0;
+            public static int middleLeft = 1;
+            public static int backLeft = 2;
+            public static int frontRight = 3;
+            public static int middleRight = 4;
+            public static int backRight = 5;
+        }
+    }
+    public static class Generic {
+        public static int timeoutMs = 3000;
+    }
 }
